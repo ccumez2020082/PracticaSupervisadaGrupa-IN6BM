3a2a74a3dfdfa6e86cc5d432d34ad0b75019f648
ccumez2020082 <ccumez-2020082@kinal.edu.gt>
2022-03-23 16:51:53 -0600

    cristian

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrarComponent } from './components/registrar/registrar.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'login', component:LoginComponent},
   {path: 'registro', component:RegistrarComponent},
   {path: 'inicio', component:InicioComponent},
+  {path: 'dashboard', component:DashboardComponent}
 
 
 ];
